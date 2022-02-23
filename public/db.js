f198b86b0838c8c513c7c30f599453a56e0a97ed
Kieran Harris <kharris19063@gmail.com>
2022-02-23 01:18:19 -0500

    reworked code, site should function as intended

--- public/db.js
+++ public/db.js
@@ -23,12 +23,44 @@ request.onsuccess = function (e) {
   }
 };
 
-
-// checks iDB exists
-function checkDatabase() {
-  console.log("checking if db executed")
+//saves record if user is offline
+const saveRecord = (record) => {
+  console.log("Offline records saved");
+  const transaction = db.transaction(["budgetStore"], "readwrite")
+  const store = transaction.objectStore("budgetStore");
+  store.add(record)
 }
 
+
+// checks if DB exists
+function checkDatabase() {
+  console.log("checking if db executed")
+  const transaction = db.transaction(["budgetStore"], "readwrite")
+  const store = transaction.objectStore('budgetStore');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function(){
+    if(getAll.result.length > 0) {
+      fetch('/api/transation/bulk', {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Conetent-Type": "application/json",
+        },
+      })
+      .then((response)=>{
+        return response.json();
+      })
+      .then(()=>{
+        const transaction = db.transaction(["budgetStore"], "readwrite")
+        const store = transaction.objectStore('budgetStore');
+        store.clear();
+        console.log("Cleared store");
+      });
+    }
+  };
+}
 
 // Listen for app coming back online
 window.addEventListener('online', checkDatabase);
